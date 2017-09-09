f4be0968214de1f9bd43cb60aa03a3021191e97f
Izodrai <valentinperraudin@gmail.com>
2017-09-09 12:22:41 +0200

    remove old files for symfony refactor

---
+++ README.md
@@ -0,0 +1,4 @@
+Ashbringer
+==========
+
+A Symfony project created on September 9, 2017, 10:14 am.
--- tools/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function load_config() {
-  return json_decode(file_get_contents("../config/general_config.json"));
-}
-
- ?>
